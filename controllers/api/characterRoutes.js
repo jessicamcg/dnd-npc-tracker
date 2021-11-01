ba062e6e40a8cdb31f3d7ee5d393225bcef428cf
jessicamcg <jessicamcguico@gmail.com>
2021-11-01 14:49:01 -0500

    savin commit

--- controllers/api/characterRoutes.js
+++ controllers/api/characterRoutes.js
@@ -1,22 +1,47 @@
 const router = require('express').Router();
 const { Character, DM } = require('../../models');
 
-router.post('/', async (req, res) => {
+// router.post('/', async (req, res) => {
+//     try {
+//         router.get('/', async (req, res) => {
+//             const DMData = await DM.findOne({
+//                 // need work
+//             })
+//         })
+//         const characterData = await Character.create({
+//             dm_id_fk: DMData,
+//             race: req.body.race,
+//             class: req.body.class,
+//             notes: req.body.notes,
+//       });
+//       res.status(200).json(characterData);
+//     } catch (err) {
+//       res.status(400).json(err);
+//     }
+// });
+
+router.post('/:id', async (req, res) => {
     try {
-        router.get('/', async (req, res) => {
-            const DMData = await DM.findOne({
-                // need work
-            })
+        
+        const DMData = await DM.findOne({
+            where:{
+              id: req.params.id
+            },
         })
+
+        if(!DMData){
+            res.status(404).json({message: 'No DM with that id'})
+        }
+        
         const characterData = await Character.create({
-            dm_id_fk: DMData,
+            dm_id_fk: DMData.dataValues.id,
             race: req.body.race,
             class: req.body.class,
             notes: req.body.notes,
-      });
-      res.status(200).json(characterData);
+        });
+        res.status(200).json(characterData);
     } catch (err) {
-      res.status(400).json(err);
+      res.status(500).json(err);
     }
 });
 
