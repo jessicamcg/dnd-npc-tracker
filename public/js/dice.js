52f36ff6f5a10d34d12bb10b3dff8450004acac8
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-11-03 14:14:02 -0500

    This commit adds the post fetch route

--- public/js/dice.js
+++ public/js/dice.js
@@ -1,19 +1,15 @@
-                // const { DiceRoller, DiceRoll } = require('rpg-dice-roller');
-// import { DiceRoll } from 'rpg-dice-roller';
-// const numberOfRolls = document.querySelector('.rolls').value;
-// const dice = document.querySelector('.die').value;
-// const dice_button = document.querySelector('.dice-roller_button')
-// const results = document.querySelector('.results')
+const numberOfRolls = document.querySelector('.rolls');
+const dice = document.querySelector('.die').value;
+const dice_button = document.querySelector('.dice-roller_button')
 
 
-const rollDice = (number, type) => {
-    console.log('hey');
-    const numberStr = number.toString()
-    const roll = new DiceRoll(`${numberStr}d${type}`)
-    console.log(roll.output)
-    // results.innerHTML = roll.output;
-}
+dice_button.addEventListener('click', response);
 
-// dice_button.addEventListener('click', rollDice(numberOfRolls,dice));
 
-rollDice(5,"12")
+const response = await fetch(`dice/:number/:type`, {
+    method: "POST",
+    body: JSON.stringify({
+        numOfRolls: numberOfRolls,
+        Die: dice,
+    })
+});
