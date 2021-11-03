2a6d46b741a813e2f20c4f17f56e5b299a3a1c3f
Juri <jasonluxie@gmail.com>
2021-11-03 18:03:20 -0500

    logic working, connecting to html

--- public/js/diceRandomizerLogic.js
+++ public/js/diceRandomizerLogic.js
@@ -7,27 +7,33 @@ const characterRandomizer = async () => {
         // console.log(response)
         let singleArr = [];
         const responseParsed = await response.json();
-        console.log(responseParsed);
-        console.log(responseParsed[0].rolls[0].value);
         if (responseParsed) {
             for (let j = 0; j < responseParsed[0].rolls.length; j++) {
                 singleArr.push(responseParsed[0].rolls[j].value);
             }
-            console.log("singleArr:", singleArr);
             let sortedArr = singleArr.sort((a, b) => a - b);
-            console.log("sortedArr:", sortedArr);
             sortedArr.shift();
-            console.log("shifted sortedArr:", sortedArr);
             let sum = 0;
             for (let k = 0; k < sortedArr.length; k++) {
                 sum = sum + sortedArr[k];
             }
-            statArr.push(sum);
         }
-    }
-    console.log("finalArr:", statArr);
-};
+        console.log("finalArr:", statArr);
+        const strength = document.querySelector("#strength");
+        const dexterity = document.querySelector("#dexterity");
+        const constitution = document.querySelector("#constitution");
+        const intelligence = document.querySelector("#intelligence");
+        const wisdom = document.querySelector("#wisdom");
+        const charisma = document.querySelector("#charisma");
 
+        strength.setAttribute("value", statArr[0]);
+        dexterity.setAttribute("value", statArr[1]);
+        constitution.setAttribute("value", statArr[2]);
+        intelligence.setAttribute("value", statArr[3]);
+        wisdom.setAttribute("value", statArr[4]);
+        charisma.setAttribute("value", statArr[5]);
+    }
+};
 document
     .querySelector(".character-creation-form_randomizer")
     .addEventListener("click", characterRandomizer);
