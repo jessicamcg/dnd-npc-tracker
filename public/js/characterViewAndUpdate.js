36dcf4dc309bb6267f93d29deb90ea0431aa7ddd
jessicamcg <jessicamcguico@gmail.com>
2021-11-03 11:04:25 -0500

    character cards moved to my-characters

--- public/js/characterViewAndUpdate.js
+++ public/js/characterViewAndUpdate.js
@@ -1,4 +1,4 @@
-const modifierCalc = require("../../utils/helpers");
+// const modifierCalc = require(".../utils/helpers.js");
 const characterView = async (id) => {
     const characterName = document.querySelector("#character-name");
     const strength = document.querySelector("#strength");
@@ -9,12 +9,12 @@ const characterView = async (id) => {
     const charisma = document.querySelector("#charisma");
     const notes = document.querySelector("#notes");
 
-    const strengthLabel = docutment.querySelector("#strength-label");
-    const dexterityLabel = docutment.querySelector("#dexterity-label");
-    const constitutionLabel = docutment.querySelector("#constitution-label");
-    const intelligenceLabel = docutment.querySelector("#intelligence-label");
-    const wisdomLabel = docutment.querySelector("#wisdom-label");
-    const charismaLabel = docutment.querySelector("#charisma-label");
+    const strengthLabel = document.querySelector("#strength-label");
+    const dexterityLabel = document.querySelector("#dexterity-label");
+    const constitutionLabel = document.querySelector("#constitution-label");
+    const intelligenceLabel = document.querySelector("#intelligence-label");
+    const wisdomLabel = document.querySelector("#wisdom-label");
+    const charismaLabel = document.querySelector("#charisma-label");
 
     const response = await fetch(`/api/character/${id}`, {
         method: "GET",
@@ -49,8 +49,8 @@ const characterView = async (id) => {
                 halfElf.setAttribute("selected", "");
                 break;
             case "Halfling":
-                const halfing = document.querySelector("#Halfing");
-                halfing.setAttribute("selected", "");
+                const halfling = document.querySelector("#Halfling");
+                halfling.setAttribute("selected", "");
                 break;
             case "Half-Orc":
                 const halfOrc = document.querySelector("#Half-Orc");
